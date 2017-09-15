4b88fa8d4ee1326d94c8d9574cea6b638b066a23
Peter Sorensen <peter.sorensen@berkeley.edu>
2017-09-15 08:37:34 -0700

    extra credit

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.4.13;
 contract greeter {
 	/* Add one variable to hold our greeting */
 	string greeting;
+	uint numGreetings = 0;
 
 	function greeter(string _greeting) public {
 		/* Write one line of code for the contract to set our greeting */
@@ -12,6 +13,7 @@ contract greeter {
 
 	function greet() constant returns (string)  {
 		/* Write one line of code to allow the contract to return our greeting */
+		numGreetings++;
 		return greeting;
 	}
 }
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
+    event calculated(uint fibNumber);
+
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
 		uint[] memory fibNums = new uint[](position + 2);
@@ -10,6 +12,7 @@ contract Fibonacci {
 		for (uint i = 2; i <= position; i++) {
 		    fibNums[i] = fibNums[i-1] + fibNums[i-2];
 		}
+		calculated(fibNums[position]);
 		return fibNums[position];
 	}
 
