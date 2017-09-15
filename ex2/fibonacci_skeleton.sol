edfc98f14a163fb81cc3767ef33ae73b7a1ff0c4
Peter Sorensen <peter.sorensen@berkeley.edu>
2017-09-15 08:11:25 -0700

    updated hw1

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -4,7 +4,15 @@ pragma solidity ^0.4.16;
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
+		uint[] memory fibNums = new uint[](position + 2);
+		fibNums[0] = 0;
+		fibNums[1] = 1;
+		for (uint i = 2; i <= position; i++) {
+		    fibNums[i] = fibNums[i-1] + fibNums[i-2];
+		}
+		return fibNums[position];
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function () {}
 }
